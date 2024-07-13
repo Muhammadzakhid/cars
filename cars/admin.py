6d844cb9228c901836a2db6b2534ab3383bc10e2
Muhammadzakhid <m92817516@gmail.com>
2024-07-13 19:01:35 +0500

    Cars model finished

---
+++ cars/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Car
+
+# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'color', 'description')
+    search_fields = ('title', 'color')
+    list_filter = ('color',)
+
+admin.site.register(Car, CarAdmin)
